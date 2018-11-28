9d94793eb5a41ba704c00ee40c36ccc740dcb50f
dcmartin <github@dcmartin.com>
2018-11-28 13:52:49 -0800

    Architecture

--- setup/hzn-install.sh
+++ setup/hzn-install.sh
@@ -24,7 +24,7 @@ if [ "${ARCH}" == "aarch64" ]; then
   ARCH="arm64"
 elif [ "${ARCH}" == "x86_64" ]; then
   ARCH="amd64"
-else if [ "${ARCH}" == "armv7l" ]; then
+elif [ "${ARCH}" == "armv7l" ]; then
   ARCH="arm64"
 else
   ARCH=$(arch)
