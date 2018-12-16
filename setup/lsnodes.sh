e306aeb11852a96dfe0cc505ab528b3e0618f4a8
dcmartin <github@dcmartin.com>
2018-12-15 21:21:27 -0800

    initial commit; list nodes in organization based on configuration

---
+++ setup/lsnodes.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+if [ -z $(command -v jq) ]; then
+  echo "*** ERROR $0 $$ -- please install jq"
+  exit 1
+fi
+
+CONFIG="horizon.json"
+if [ -z "${1}" ]; then
+  if [ -s "${CONFIG}" ]; then
+    echo "+++ WARN $0 $$ -- no configuration specified; default found: ${CONFIG}" &> /dev/stderr
+  else
+    echo "*** ERROR $0 $$ -- no configuration specified; no default: ${CONFIG}; run mkconfig.sh" &> /dev/stderr
+    exit 1
+  fi
+else
+  CONFIG="${1}"
+fi
+if [ ! -s "${CONFIG}" ]; then
+  echo "*** ERROR $0 $$ -- configuration file empty: ${1}" &> /dev/stderr
+  exit 1
+fi
+
+IBM_CLOUD_APIKEY=$(jq -r '.exchanges[]|select(.id=="alpha")|.password' "${CONFIG}")
+if [ -z "${IBM_CLOUD_APIKEY}" ] || [ "${IBM_CLOUD_APIKEY}" == "null" ]; then
+  echo "*** ERROR $0 $$ -- invalid IBM_CLOUD_APIKEY" &> /dev/stderr
+  exit 1
+fi
+  
+HZN_EXCHANGE_URL=$(jq -r '.exchanges[]|select(.id=="alpha")|.url' "${CONFIG}")
+if [ -z "${HZN_EXCHANGE_URL}" ] || [ "${HZN_EXCHANGE_URL}" == "null" ]; then
+  echo "*** ERROR $0 $$ -- invalid HZN_EXCHANGE_URL" &> /dev/stderr
+  exit 1
+fi
+
+IBM_CLOUD_LOGIN_EMAIL=$(jq -r '.exchanges[]|select(.id=="alpha")|.org' "${CONFIG}")
+if [ -z "${IBM_CLOUD_LOGIN_EMAIL}" ] || [ "${IBM_CLOUD_LOGIN_EMAIL}" == "null" ]; then
+  echo "*** ERROR $0 $$ -- invalid IBM_CLOUD_LOGIN_EMAIL" &> /dev/stderr
+  exit 1
+fi
+
+ALL=$(curl -sL -u "${IBM_CLOUD_LOGIN_EMAIL}/iamapikey:${IBM_CLOUD_APIKEY}" "${HZN_EXCHANGE_URL}/orgs/${IBM_CLOUD_LOGIN_EMAIL}/nodes")
+NODES=$(echo "${ALL}" | jq '{"nodes":[.nodes | objects | keys[]] | unique}' | jq -r '.nodes[]')  
+OUTPUT='{"nodes":['
+i=0; for NODE in ${NODES}; do 
+  if [[ $i > 0 ]]; then OUTPUT="${OUTPUT}"','; fi
+  OUTPUT="${OUTPUT}"$(echo "${ALL}" | jq '.nodes."'"${NODE}"'"' | jq -c '.id="'"${NODE}"'"')
+  i=$((i+1))
+done 
+OUTPUT="${OUTPUT}"']}'
+echo "${OUTPUT}" | jq -c '.'
