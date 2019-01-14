93f525a6e1494b79c9e7dd45191ecb46f4f983d8
dcmartin <github@dcmartin.com>
2019-01-13 19:58:20 -0800

    dev

--- hal/rootfs/usr/bin/lscpu.sh
+++ hal/rootfs/usr/bin/lscpu.sh
@@ -3,13 +3,13 @@ if [ -z $(command -v "lscpu") ]; then
   echo '{"lscpu":null}'
   exit 1
 fi
-echo -n '{"lscpu":['
+echo -n '{"lscpu":{'
 lscpu | while read -r; do
   KEY=$(echo "$REPLY" | sed 's|^\([^:]*\):.*|\1|')
   VAL=$(echo "$REPLY" | sed 's|^[^:]*:[ \t]*\(.*\)|\1|')
   if [ -n "${VALUE:-}" ]; then echo -n ','; fi
-  VALUE='{"'${KEY}'": "'${VAL}'"}'
+  VALUE='"'${KEY}'": "'${VAL}'"'
   echo -n "${VALUE}"
 done
-echo ']}'
+echo '}}'
 exit 0
