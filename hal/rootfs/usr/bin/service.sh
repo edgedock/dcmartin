2ef0b856d4a36088f0cb141e3b78cc52e28a9036
dcmartin <github@dcmartin.com>
2019-01-21 08:54:40 -0800

    dev

--- hal/rootfs/usr/bin/service.sh
+++ hal/rootfs/usr/bin/service.sh
@@ -2,7 +2,6 @@
 
 # Get the currect CPU consumption, then construct the HTTP response message
 HEADERS="Content-Type: application/json; charset=ISO-8859-1"
-PATH="${PATH}:."
 BODY='{"lshw":'$(lshw.sh|jq '.lshw')',"lsusb":'$(lsusb.sh|jq '.lsusb?')',"lscpu":'$(lscpu.sh|jq '.lscpu?')',"lspci":'$(lspci.sh|jq '.lspci?')',"lsblk":'$(lsblk.sh|jq '.lsblk?')'}'
 HTTP="HTTP/1.1 200 OK\r\n${HEADERS}\r\n\r\n${BODY}\r\n"
 
