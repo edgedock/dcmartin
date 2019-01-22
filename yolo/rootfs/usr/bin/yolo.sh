8a2297fb88c079115c9b73625f7f82b4603efaf6
David C. Martin <dcmartin@us.ibm.com>
2019-01-22 12:17:16 -0800

    dev

--- yolo/rootfs/usr/bin/yolo.sh
+++ yolo/rootfs/usr/bin/yolo.sh
@@ -2,6 +2,7 @@
 fswebcam /tmp/image.$$.jpg
 /darknet/darknet detector test cfg/voc.data cfg/yolov2-tiny-voc.cfg yolov2-tiny-voc.weights /tmp/image.$$.jpg > /tmp/image.$$.out
 TIME=$(cat /tmp/image.$$.out | egrep "Predicted" | sed 's/.*Predicted in \([^ ]*\).*/\1/')
+if [ -z "${TIME}" ]; then TIME=0; fi
 PERSONS=$(cat /tmp/image.$$.out | egrep '^person' | wc -l)
 NODEID=$(hzn node list | jq -r '.id')
-echo '{"devid":"'${NODEID}'","date":$(date +%s),"time":'${TIME}',"person":'${PERSONS}'}'
+echo '{"devid":"'${NODEID}'","date":'$(date +%s)',"time":'${TIME}',"person":'${PERSONS}'}'
