c827dfbdc6f39dd96912dd668aa7d4150a4298b3
David C. Martin <dcmartin@us.ibm.com>
2019-01-22 13:20:47 -0800

    dev

--- yolo/rootfs/usr/bin/yolo.sh
+++ yolo/rootfs/usr/bin/yolo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 fswebcam /tmp/image.$$.jpg
-/darknet/darknet detector test cfg/voc.data cfg/yolov2-tiny-voc.cfg yolov2-tiny-voc.weights /tmp/image.$$.jpg > /tmp/image.$$.out
+cd /darknet && ./darknet detector test cfg/voc.data cfg/yolov2-tiny-voc.cfg yolov2-tiny-voc.weights /tmp/image.$$.jpg > /tmp/image.$$.out
 TIME=$(cat /tmp/image.$$.out | egrep "Predicted" | sed 's/.*Predicted in \([^ ]*\).*/\1/')
 if [ -z "${TIME}" ]; then TIME=0; fi
 PERSONS=$(cat /tmp/image.$$.out | egrep '^person' | wc -l)
