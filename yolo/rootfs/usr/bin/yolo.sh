285255668780cf8c584c946659759bc5d3f89278
dcmartin <github@dcmartin.com>
2019-01-22 18:10:16 -0800

    Changed to include mock result from personx4.jpg

--- yolo/rootfs/usr/bin/yolo.sh
+++ yolo/rootfs/usr/bin/yolo.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 fswebcam --no-banner /tmp/image.$$.jpg
+if [ ! -s /tmp/image.$$.jpg ]; then cp /darknet/data/personx4.jpg /tmp/image.$$.jpg; MOCK=true; else MOCK=false; fi
 cd /darknet && ./darknet detector test cfg/voc.data cfg/yolov2-tiny-voc.cfg yolov2-tiny-voc.weights /tmp/image.$$.jpg > /tmp/image.$$.out
 TIME=$(cat /tmp/image.$$.out | egrep "Predicted" | sed 's/.*Predicted in \([^ ]*\).*/\1/')
 if [ -z "${TIME}" ]; then TIME=0; fi
 PERSONS=$(cat /tmp/image.$$.out | egrep '^person' | wc -l)
 NODEID=$(hostname)
 IMAGE=$(base64 -w 0 -i predictions.jpg)
-echo '{"devid":"'${NODEID}'","date":'$(date +%s)',"time":'${TIME}',"person":'${PERSONS}',"image":"'${IMAGE}'"}'
+echo '{"devid":"'${NODEID}'","date":'$(date +%s)',"time":'${TIME}',"person":'${PERSONS}',"image":"'${IMAGE}'","mock":"'${MOCK}'"}'
+rm -f /tmp/image.$$.jpg /tmp/image.$$.out predictions.jpg
