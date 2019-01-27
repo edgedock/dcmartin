244c69a5d82ea32bfdc1bd297c7b7564add7a410
dcmartin <github@dcmartin.com>
2019-01-27 23:26:18 +0000

    dev

--- motion/rootfs/usr/bin/motion.sh
+++ motion/rootfs/usr/bin/motion.sh
@@ -3,11 +3,11 @@
 # TMP
 if [ -d '/tmpfs' ]; then TMP='/tmpfs'; else TMP='/tmp'; fi
 
-if [ -n "${MOTION_LOG_LEVEL}" ]; then MOTION_LOG_LEVEL=2; fi
-if [ -n "${MOTION_LOG_TYPE}" ]; then MOTION_LOG_TYPE="all"; fi
+if [ -z "${MOTION_LOG_LEVEL:-}" ]; then MOTION_LOG_LEVEL=2; fi
+if [ -z "${MOTION_LOG_TYPE:-}" ]; then MOTION_LOG_TYPE="all"; fi
 
 # mkdir -p ${TMP}/motion
 # ln -s ${TMP}/motion /var/lib/motion 
 
 # start motion
-motion -b -d ${MOTION_LOG_LEVEL} -k ${MOTION_LOG_TYPE} -c /etc/motion/motion.conf -l /dev/stderr
+motion -b -n ${MOTION_LOG_LEVEL} -k ${MOTION_LOG_TYPE} -c /etc/motion/motion.conf -l /dev/stderr
