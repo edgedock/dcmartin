2f7e1e7c0a506b4e88e03de73ae88a3d371e1784
dcmartin <github@dcmartin.com>
2019-02-01 06:32:33 -0800

    removed HZN_DEVICE_ID

--- docker-run.sh
+++ docker-run.sh
@@ -61,10 +61,6 @@ else
   echo "+++ WARN $0 $$ -- no tmpfs"
 fi
 
-# pre-defined inputs
-HZN_DEVICE_ID=$(hostname)-${DOCKER_NAME}
-OPTIONS="${OPTIONS:-}"' -e HZN_DEVICE_ID='"${HZN_DEVICE_ID}"
-
 # inputs
 if [ "$(jq '.userInput!=null' ${SERVICE})" == 'true' ]; then
   URL=$(jq -r '.url' ${SERVICE})
