bb1f066acd7998f6f5de642932d51889446ab2d0
dcmartin <github@dcmartin.com>
2019-02-03 20:20:36 -0800

    dev

--- test-service.sh
+++ test-service.sh
@@ -3,5 +3,9 @@
 if [ -z "${SERVICE_LABEL:-}" ]; then SERVICE_LABEL=$(echo "${0##*/}" | sed 's|test-\(.*\).sh|\1|'); fi
 
 REPLY=$(cat -)
-
-echo "${REPLY}" | jq -c '.'${SERVICE_LABEL}'!=null'
+if [ ! -z "${REPLY}" ]; then 
+  echo "${REPLY}" | jq -c '.'${SERVICE_LABEL}'!=null'
+else
+  echo "no input" &> /dev/stderr
+  exit 1
+fi
