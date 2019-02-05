861b78a9c8de392b63600a796a87cf50f22ddb9c
dcmartin <github@dcmartin.com>
2019-02-05 14:35:03 -0800

    lgtm

--- yolo2msghub/kafkacat.sh
+++ yolo2msghub/kafkacat.sh
@@ -12,32 +12,53 @@ kafkacat -E -u -C -q -o end -f "%s\n" -b "${BROKER}" \
   -t "${TOPIC}" | while read -r; do
     if [ -n "${REPLY}" ]; then
       echo "${REPLY}" >> $0.$$.out
-      echo "${REPLY}" | ./test-yolo2msghub.sh
+      VALID=$(echo "${REPLY}" | ./test-yolo2msghub.sh 2> /dev/null)
     else
+      echo "+++ WARN $0 $$ -- null payload"
+      continue
+    fi
+    if [ "${VALID}" != 'true' ]; then
+      echo "+++ WARN $0 $$ -- invalid payload: ${REPLY}"
       continue
     fi
     ID=$(echo "${REPLY}" | jq -r '.hzn.device_id')
     DATE=$(echo "${REPLY}" | jq -r '.yolo2msghub.yolo.date')
     THIS=$(echo "${DEVICES}" | jq '.[]|select(.id=="'${ID}'")')
     if [ -z "${THIS}" ] || [ "${THIS}" == 'null' ]; then
-      THIS='{"id":"'${ID}'","date":'${DATE}'}'
+      THIS='{"id":"'${ID}'","date":'${DATE}',"count":0}'
       DEVICES=$(echo "${DEVICES}" | jq '.+=['"${THIS}"']')
-      echo "${DEVICES}"
+      TOTAL=0
       LAST=0
     else
-      echo LAST=$(echo "${THIS}" | jq '.date')
+      TOTAL=$(echo "${THIS}" | jq '.count')
+      LAST=$(echo "${THIS}" | jq '.date')
     fi
+
+    DEVICES=$(echo "${DEVICES}" | jq '(.[]|select(.id=="'${ID}'"))|='"${THIS}")
+
     if [ $(echo "${REPLY}" | jq '.yolo2msghub.yolo!=null') == 'true' ]; then
       if [ $(echo "${REPLY}" | jq -r '.yolo2msghub.yolo.mock') != 'true' ]; then
         if [ ${DATE} -gt ${LAST} ]; then
-          DATE=${LAST}
-          if [ $(echo "${REPLY}" | jq -r '.yolo2msghub.yolo.count') -gt 0 ]; then
+	  COUNT=$(echo "${REPLY}" | jq -r '.yolo2msghub.yolo.count')
+          if [ ${COUNT} -gt 0 ]; then
+	    echo "--- INFO $0 $$ -- ${ID} at ${DATE}: person count ${COUNT}"
+            TOTAL=$((${TOTAL}+${COUNT}))
+            THIS=$(echo "${THIS}" | jq '.count='${TOTAL})
             echo "${REPLY}" | jq -r '.yolo2msghub.yolo.image' | base64 --decode > $0.$$.jpeg
             if [ ! -z $(command -v open) ]; then open $0.$$.jpeg; fi
+	  else
+	    echo "+++ WARN $0 $$ -- ${ID} at ${DATE}: no person"
           fi
+          THIS=$(echo "${THIS}" | jq '.date='${DATE})
+	  DEVICES=$(echo "${DEVICES}" | jq '(.[]|select(.id=="'${ID}'"))|='"${THIS}")
+          DEVICES=$(echo "${DEVICES}" | jq '.|sort_by(.count)|reverse')
         fi
+      else
+	echo "+++ WARN $0 $$ -- ${ID} at ${DATE}: mock"
       fi
+    else
+      echo "+++ WARN $0 $$ -- ${ID} at ${DATE}: no yolo output"
     fi
-    echo "ALL:" $(jq -r '.hzn.device_id' $0.$$.out | sort | uniq)
+    echo "${DEVICES}" | jq -c '.'
 done
 rm -f $0.$$.*
