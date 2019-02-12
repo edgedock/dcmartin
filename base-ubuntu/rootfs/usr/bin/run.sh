5636af7712b644eb7723e6ca129bee69abea7c4f
dcmartin <github@dcmartin.com>
2019-02-12 14:42:31 -0800

    lgtm

--- base-ubuntu/rootfs/usr/bin/run.sh
+++ base-ubuntu/rootfs/usr/bin/run.sh
@@ -6,13 +6,8 @@ if [ -d '/tmpfs' ]; then TMP='/tmpfs'; else TMP='/tmp'; fi
 # hzn config
 export HZN='{"hzn":{"agreementid":"'${HZN_AGREEMENTID}'","arch":"'${HZN_ARCH}'","cpus":'${HZN_CPUS:-0}',"device_id":"'${HZN_DEVICE_ID}'","exchange_url":"'${HZN_EXCHANGE_URL}'","host_ips":['$(echo "${HZN_HOST_IPS}" | sed 's/,/","/g' | sed 's/\(.*\)/"\1"/')'],"organization":"'${HZN_ORGANIZATION}'","pattern":"'${HZN_PATTERN}'","ram":'${HZN_RAM:-0}'},"date":'$(date +%s)',"service":"'${SERVICE_LABEL:-}'"}'
 
-# add hostname
-IPADDR=$(hostname -i | awk '{ print $1 }' | awk -F\. '{ printf("%03d%03d%03d%03d\n", $1, $2, $3, $4) }')
-HOSTNAME="$(hostname)-${IPADDR}"
-HZN=$(echo "${HZN}" | jq '.hostname="'${HOSTNAME}'"')
-
 # make a file
-echo "${HZN}" > ${TMP}/config.json
+echo "${HZN}" > "${TMP}/config.json"
 
 # label
 if [ ! -z "${SERVICE_LABEL:-}" ] && [ ! -z $(command -v "${SERVICE_LABEL:-}.sh" ) ]; then
--- yolo2msghub/rootfs/usr/bin/service.sh
+++ yolo2msghub/rootfs/usr/bin/service.sh
@@ -4,17 +4,28 @@
 if [ -d '/tmpfs' ]; then TMP='/tmpfs'; else TMP='/tmp'; fi
 
 # hzn config
-if [ ! -z "${HZN}" ]; then
-  BODY="${HZN}"
-else
-  echo "*** ERROR $0 $$ -- environment HZN unset; exiting" 2> /dev/stderr
-  exit 1
+if [ -z "${HZN}" ]; then
+  if [ ! -s "/tmp/config.json" ]; then
+    echo "*** ERROR $0 $$ -- environment HZN unset; empty /tmp/config.json; exiting" 2> /dev/stderr
+    exit 1
+  fi
+  echo "+++ WARN $0 $$ -- environment HZN unset; using /tmp/config.json; continuing" 2> /dev/stderr
+  export HZN=$(jq '.' "/tmp/config.json")
+  if [ -z "${HZN}" ]; then
+    echo "*** ERROR $0 $$ -- environment HZN unset; invalid /tmp/config.json; exiting" $(cat /tmp/config.json) 2> /dev/stderr
+    exit 1
+  fi
 fi
+
+BODY="${HZN}"
 
 # git pid
 if [ ! -z "${SERVICE_LABEL:-}" ]; then
-  PID=$(ps | grep "${SERVICE_LABEL:-}.sh" | grep -v grep | awk '{ print $1 }' | head -1)
-  if [ -z "${PID}" ]; then PID=0; fi
+  CMD=$(command -v "${SERVICE_LABEL:-}.sh")
+  if [ ! -z "${CMD}" ]; then
+    PID=$(ps | grep "${CMD}" | grep -v grep | awk '{ print $1 }' | head -1)
+  fi
+  if [ -z "${PID:-}" ]; then PID=0; fi
   BODY=$(echo "${BODY}" | jq '.pid='"${PID}")
 else
   echo "*** ERROR $0 $$ -- no SERVICE_LABEL; exiting" 2> /dev/stderr
