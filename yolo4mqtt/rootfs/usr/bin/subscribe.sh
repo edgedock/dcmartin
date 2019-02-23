2cf28af901cd8ef94b9af7fd84ed26a3aba12582
dcmartin <github@dcmartin.com>
2019-02-23 08:06:16 -0800

    remove

--- yolo4mqtt/rootfs/usr/bin/subscribe.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-JPG="${1}"
-# get image
-if [ "${DEBUG:-}" == 'true' ]; then echo "??? DEBUG $0 $$ -- listening to ${YOLO4MQTT_HOST} on topic: ${YOLO4MQTT_TOPIC}" &> /dev/stderr; fi
-mosquitto_sub -h ${YOLO4MQTT_HOST} -t ${YOLO4MQTT_TOPIC} 2> /dev/stderr | base64 | base64 --decode > "${JPG}"
