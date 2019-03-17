55d063a8f9e290ff8279271128a61aa364a14421
David C. Martin <dcmartin@us.ibm.com>
2019-03-16 20:42:19 -0700

    lgtm

--- scripts/read.sh
+++ scripts/read.sh
@@ -28,8 +28,11 @@ LIGHT_PURPLE='\033[1;35m'
 LIGHT_CYAN='\033[1;36m'
 WHITE='\033[1;37m'
 
-cat "${SCRIPT}" | while read; do 
-  echo -e -n "${LIGHT_CYAN}$(hostname) $(date +%T)"' $ '"${YELLOW}"
+PROMPT="${LIGHT_CYAN}"
+COMMAND="${LIGHT_BLUE}"
+
+cat "${SCRIPT}" | while read -r; do 
+  echo -e -n "${PROMPT}$(hostname) $(date +%T)"' $ '"${COMMAND}"
   for ((i=0; i<${#REPLY}; i++)); do echo "after 200" | tclsh; printf "${REPLY:$i:1}"; done
   echo -e "${NC}"
   eval "${REPLY}"
