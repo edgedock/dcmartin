9feaf80fa0cd32bd429ae7ae4f8d699bc6ac6230
dcmartin <github@dcmartin.com>
2019-03-28 08:40:01 -0700

    lgtm

--- setup/aptget-horizon.sh
+++ setup/aptget-horizon.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+TYPE=linux
+DIST=ubuntu
+RELEASE=xenial
 REPO=updates
 LIST=/etc/apt/sources.list.d/bluehorizon.list
 URL=http://pkg.bluehorizon.network
 KEY=${URL}/bluehorizon.network-public.key
 wget -qO - ${KEY} | sudo apt-key add -
-echo "deb [arch=armhf,arm64,amd64] ${URL}/linux/ubuntu xenial-${REPO} main" > /tmp/$$ && sudo mv /tmp/$$ ${LIST}
-sudo apt-get update -y && sudo apt-get install -y bluehorizon
+echo "deb [arch=armhf,arm64,amd64,ppc64el] ${URL}/${TYPE}/${DIST} ${RELEASE}-${REPO} main" > /tmp/$$ && sudo mv /tmp/$$ ${LIST}
+sudo apt-get update -qq && sudo apt-get install -y -qq bluehorizon
