81cfbe56ffbeab5b38a6fbfcd3e2889d345129da
dcmartin <github@dcmartin.com>
2019-04-05 21:42:59 -0700

    lgtm

--- setup/aptget-horizon.sh
+++ setup/aptget-horizon.sh
@@ -1,12 +1,43 @@
 #!/bin/bash
-TYPE=linux
-DIST=ubuntu
-#RELEASE=$(lsb_release -cs)
-RELEASE=xenial
-REPO=updates
-LIST=/etc/apt/sources.list.d/bluehorizon.list
-URL=http://pkg.bluehorizon.network
-KEY=${URL}/bluehorizon.network-public.key
-wget -qO - ${KEY} | sudo apt-key add -
-sudo add-apt-repository "deb [arch=armhf,arm64,amd64,ppc64el] ${URL}/${TYPE}/${DIST} ${RELEASE}-${REPO} main"
-sudo apt-get update -qq && sudo apt-get install -y -qq --no-install-recommend bluehorizon
+
+HZN_URL="http://pkg.bluehorizon.network/"
+  
+if [ "${VENDOR:-}" != "apple" ] && [ "${OSTYPE:-}" != "darwin" ]; then
+  TYPE=linux
+  if [ ! -z $(command -v "apt") ]; then
+    DIST=ubuntu
+    #RELEASE=$(lsb_release -cs)
+    RELEASE=xenial
+    REPO=updates
+    KEY=${HZN_URL}/bluehorizon.network-public.key
+    wget -qO - ${KEY} | sudo apt-key add -
+    sudo add-apt-repository "deb [arch=armhf,arm64,amd64,ppc64el] ${HZN_URL}/${TYPE}/${DIST} ${RELEASE}-${REPO} main"
+    sudo apt-get update -qq && sudo apt-get install -y -qq --no-install-recommend bluehorizon
+  else
+    echo "Error: cannot locate apt command" &> /dev/stderr
+  fi
+else
+  VERSION=$(curl -fsSL 'http://pkg.bluehorizon.network/macos/' | egrep 'horizon-cli' | sed 's/.*-cli-\(.*\)\.pkg<.*/\1/' | sort | uniq | tail -1)
+
+  if [ -z "${1}" ]; then echo "--- INFO -- $0 $$ -- no version specified; using ${VERSION}" &> /dev/stderr; else VERSION="${1}"; fi
+
+  HZN_PLATFORM="macos"
+  if [ ! -z $(command -v 'hzn') ]; then
+    if [ ! -z "$(hzn node list 2> /dev/null)" ]; then
+      echo "--- INFO -- $0 $$ -- unregistering" &> /dev/stderr
+      hzn unregister -f
+    fi
+  HZNCLI_PKG="horizon-cli-${VERSION}.pkg"
+  URL="${HZN_URL}/${HZN_PLATFORM}/${HZNCLI_PKG}"
+  echo "--- INFO -- $0 $$ -- getting macOS package from: ${URL}" &> /dev/stderr; fi
+  TEMPKG=$(mktemp).pkg
+  curl -fsSL "${URL}" -o "${TEMPKG}"
+  if [ -s "${TEMPKG}" ]; then
+    sudo installer -allowUntrusted -pkg "${TEMPKG}" -target /
+    horizon-container update &> /dev/null
+  else
+    echo "*** ERROR -- $0 $$ -- cannot download package from URL: ${URL}" &> /dev/stderr
+    exit 1
+  fi
+  rm -f "${TEMPKG}"
+fi
