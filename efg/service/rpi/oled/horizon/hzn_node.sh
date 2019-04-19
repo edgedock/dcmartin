c1489b5a8e6a6b31e18b031c77af2a2c6a97d033
Sanjeev Gupta <sanjeev@ibm.com>
2019-04-19 06:31:19 +0100

    Register node with a pattern

---
+++ efg/service/rpi/oled/horizon/hzn_node.sh
@@ -0,0 +1,18 @@
+#######################################
+## Individual device credentials
+#######################################
+# HZN_DEVICE_ID - An unique id of the devic e CPUID, MAC address etc.
+export HZN_DEVICE_ID='000000009fb04c00'
+
+# HZN_DEVICE_TOKEN - A devi ce token
+export HZN_DEVICE_TOKEN='w4280'
+#######################################
+
+export MYDOMAIN=com.ibm.example.efg
+export SERVICE_NAME=rpii2coled
+
+export PATTERN=sanjeev@ibm.com/rpii2coled
+
+export EVENT_STREAM_API_KEY="QfonJaAb9NVQSMqDq4qQYaZXnPBhOd74h3GoNn2oJFI5wBTI"
+
+export EXCHANGE_NODEAUTH="$HZN_DEVICE_ID:$HZN_DEVICE_TOKEN"
